ae5569f5fb1aff1184bfaf070a222815c9e6b3da
June Park <rjp2030@outlook.com>
2023-04-09 18:31:01 +0900

    Inital Commit

---
+++ src/pages/embeds/[embedid].js
@@ -0,0 +1,19 @@
+import {useRouter} from 'next/router'
+import Head from 'next/head'
+
+
+export default function Embed() {
+	const router = useRouter()
+	const {embedid} = router.query
+	
+	return (
+		<>
+		<Head>
+			<meta property="og:title" content="test">
+			<meta property="og:description" content="pythonplayer123's embed">
+			<meta property="og:image" content="https://cdn.discordapp.com/avatars/825691714383511582/ccd32bc68a5466a0c511eba28f3e0532.png?size=4096">
+			<meta property="og:url" content="https://junengames.me">
+		</Head>
+		</>
+	)
+}
