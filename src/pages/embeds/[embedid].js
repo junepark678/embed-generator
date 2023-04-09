c84086f07ec920eafdbd2392c70dcd38280f5c54
June Park <rjp2030@outlook.com>
2023-04-09 19:18:35 +0900

    now it works!

--- src/pages/embeds/[embedid].js
+++ src/pages/embeds/[embedid].js
@@ -4,15 +4,16 @@ import Head from 'next/head'
 
 export default function Embed() {
 	const router = useRouter()
-	const {embedid} = router.query
+	const {embedid, image, url, description} = router.query
 	
+
 	return (
 		<>
 		<Head>
-			<meta property="og:title" content="test"/>
-			<meta property="og:description" content="pythonplayer123's embed"/>
-			<meta property="og:image" content="https://cdn.discordapp.com/avatars/825691714383511582/ccd32bc68a5466a0c511eba28f3e0532.png?size=4096"/>
-			<meta property="og:url" content="https://junengames.me"/>
+			<meta property="og:title" content={embedid}/>
+			<meta property="og:description" content={description}/>
+			<meta property="og:image" content={image}/>
+			<meta property="og:url" content={url}/>
 		</Head>
 		</>
 	)
